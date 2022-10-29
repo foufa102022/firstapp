a9a1add072fa29b01bbd8e544ac26cf6cb635235
foufa102022 <chetouiiftikhar@gmail.com>
2022-10-29 04:59:18 +0100

    version1

--- src/app/firstap/firstap.component.ts
+++ src/app/firstap/firstap.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./firstap.component.css']
 })
 export class FirstapComponent implements OnInit {
+  name = 'ahmed chetoui'
+  link = "assets/y9.jpg"
+  
 
   constructor() { }
 
